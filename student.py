9eff0df392822dff6b8c2dc0bb8bca441d6506ec
AndrewKnaak <knaaka@gilmour.org>
2020-10-05 11:46:10 -0400

    New project start

--- student.py
+++ student.py
@@ -19,6 +19,8 @@ class Piggy(PiggyParent):
         '''
         self.LEFT_DEFAULT = 80
         self.RIGHT_DEFAULT = 80
+        self.SAFE_DISTANCE = 300
+        self.CLOSE_DISTANCE = 150
         self.MIDPOINT = 1500  # what servo command (1000-2000) is straight forward for your bot?
         self.set_motor_power(self.MOTOR_LEFT + self.MOTOR_RIGHT, 0)
         self.load_defaults()
@@ -259,12 +261,13 @@ class Piggy(PiggyParent):
         pass
 
     def nav(self):
+        """ Auto-pilot program """
         print("-----------! NAVIGATION ACTIVATED !------------\n")
         print("-------- [ Press CTRL + C to stop me ] --------\n")
         print("-----------! NAVIGATION ACTIVATED !------------\n")
         
         # TODO: build self.quick_check() that does a fast, 3-part check instead of read_distance
-        while self.read_distance() > 250:  # TODO: fix this magic number
+        while self.read_distance() > 250:
             self.fwd()
             time.sleep(.01)
         self.stop()
