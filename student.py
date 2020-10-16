1e7967ab393a4c15d4f47bc82bfd133e3ee5f4dc
AndrewKnaak <knaaka@gilmour.org>
2020-10-16 11:08:00 -0400

    For the test robot

--- student.py
+++ student.py
@@ -18,10 +18,10 @@ class Piggy(PiggyParent):
         MAGIC NUMBERS <-- where we hard-code our settings
         '''
         self.LEFT_DEFAULT = 80
-        self.RIGHT_DEFAULT = 76
+        self.RIGHT_DEFAULT = 80
         self.SAFE_DISTANCE = 400
         self.CLOSE_DISTANCE = 46
-        self.MIDPOINT = 1325  # what servo command (1000-2000) is straight forward for your bot?
+        self.MIDPOINT = 1500  # what servo command (1000-2000) is straight forward for your bot?
         self.set_motor_power(self.MOTOR_LEFT + self.MOTOR_RIGHT, 0)
         self.load_defaults()
         
