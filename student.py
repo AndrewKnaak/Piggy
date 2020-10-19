bf659dd726124096eb6c308ad81e464a882e346b
AndrewKnaak <knaaka@gilmour.org>
2020-10-19 11:31:52 -0400

    Testing scan speed

--- student.py
+++ student.py
@@ -253,7 +253,7 @@ class Piggy(PiggyParent):
     
     def scan(self):
         """Sweep the servo and populate the scan_data dictionary"""
-        for angle in range(self.MIDPOINT - 400, self.MIDPOINT + 400, 100):
+        for angle in range(self.MIDPOINT - 400, self.MIDPOINT + 400, 50):
             self.servo(angle)
             self.scan_data[angle] = self.read_distance()
         self.scan_data = OrderedDict(sorted(self.scan_data.items()))
