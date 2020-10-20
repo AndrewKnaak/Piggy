44d2ddbc10ca611755d80b7961f8ab87e125c434
AndrewKnaak <knaaka@gilmour.org>
2020-10-20 11:30:48 -0400

    Git pull

--- student.py
+++ student.py
@@ -323,7 +323,7 @@ class Piggy(PiggyParent):
         print("Will turn into clear")
         # make sure servo is straight
         self.servo(self.MIDPOINT)
-        while self.read_distance() < self.SAFE_DISTANCE + 20:
+        while self.read_distance() < self.SAFE_DISTANCE:
             self.left(primary= 60, counter= 0)
             time.sleep(.05)
         # stop motion before we end the method
